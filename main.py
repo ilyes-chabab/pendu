3bce7777bd2bbcca5ba9a3be3ca3483a290f4216
ilyes-chabab <ilyes.chabab@laplateforme.io>
2023-12-13 15:45:21 +0100

    score à faire

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 import random
-
+#init
 pygame.init()
 screen=pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Jeu du pendu")
@@ -10,23 +10,15 @@ button_text= font.render("Jouer",True,(255,255,255))
 button_rect= button_text.get_rect(center=(500,350))
 button_rect.width += 50
 button_rect.height += 20
+easy_rect= button_text.get_rect(center=(535,150))
+easy_text= font.render("easy",True,(255,255,255))
+medium_rect= button_text.get_rect(center=(525,225))
+medium_text= font.render("medium",True,(255,255,255))
+hard_rect= button_text.get_rect(center=(535,300))
+hard_text= font.render("hard",True,(255,255,255))
+start_rect= button_text.get_rect(center=(535,300))
+start_text= font.render("Commencer",True,(255,255,255))
 text_rect=pygame.Rect(225, 400,600,50)
-
-user_text= ''
-input_active=False
-
-
-pygame.display.flip()
-mot=[]
-
-with open("mots.txt","r")as file:
-    for i in file:
-        mot.append(i.rstrip("\n"))
-
-def message(message,message_rectangle,color):
-    message = font.render(message,False,color)
-    screen.blit(message,message_rectangle)    
-
 image1=pygame.image.load("picture/image1.png")
 image2=pygame.image.load("picture/image2.png")
 image3=pygame.image.load("picture/image3.png")
@@ -34,39 +26,85 @@ image4=pygame.image.load("picture/image4.png")
 image5=pygame.image.load("picture/image5.png")
 image6=pygame.image.load("picture/image6.png")
 image7=pygame.image.load("picture/image7.png")
-
-tentatives = set()  # Pour stocker les lettres déjà essayées
-random_word= random_word=random.choice(mot)
-hide_word="-"*len(random_word)
-lose=0
-win=0
+user_text= ''
+input_active=False
+tentatives = set()  # Pour stocker les lettres déjà essayées   
 game=True
 start_screen=True
+gamemode=False
+texte = True
+pygame.display.flip()
+#creation de listes pour y mettre les mots qui sont dans le fichier mots.txt, ca divise en mot faciles , moyen et difficile
+easy_word=[] # listes de mots facile - de 6 lettres
+with open("mots.txt","r")as file:
+    for i in file:
+        if len(i) <= 6:
+            easy_word.append(i.rstrip("\n"))
+medium_word=[]# listes de mots moyen entre 6 et 9 lettres
+with open("mots.txt","r")as file:
+    for i in file:
+        if len(i) > 6 and len(i) <=9:
+            medium_word.append(i.rstrip("\n"))
+hard_word=[]#llistes de mots difficiles + de 9 lettres
+with open("mots.txt","r")as file:
+    for i in file:
+        if len(i) >9 :
+            hard_word.append(i.rstrip("\n"))                        
+print(easy_word)
+print(medium_word)
+print(hard_word)
+def message(message,message_rectangle,color):
+    message = font.render(message,False,color)
+    screen.blit(message,message_rectangle)    
+
 while game:   
+    #ecran de debut pour choisir en jouer ou ajouter un mot 
     while start_screen == True:
         for event in pygame.event.get():
-            text_jouer=pygame.Rect(225,300,600,50)
-            screen.fill((0,0,255))
-            screen.blit(button_text, (button_rect.centerx -29 , button_rect.centery -10))
-            pygame.draw.rect(screen, (0,0,0), button_rect, 2)
-                        
+            screen.fill((0,130,255))
+            screen.blit(button_text, (button_rect.centerx -29 , button_rect.centery -15))
+            pygame.draw.rect(screen, (0,0,0), button_rect, 2)       
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if button_rect.collidepoint(event.pos):
-                    start_screen=False
+                    gamemode=True
+                    #si l'utilisateur appuie sur jouer , un menu avec les 3 mode de difficultés s'ouvrira
+                    while gamemode:                       
+                        for event in pygame.event.get():
+                            pygame.draw.rect(screen , (0,0,250),(350,90,350,300))
+                            screen.blit(easy_text, (easy_rect.centerx -29 , easy_rect.centery -10))
+                            screen.blit(medium_text, (medium_rect.centerx -29 , medium_rect.centery -10))
+                            screen.blit(hard_text, (hard_rect.centerx -29 ,hard_rect.centery -10))
+                            if event.type == pygame.MOUSEBUTTONDOWN:
+                                if easy_rect.collidepoint(event.pos):
+                                    liste= easy_word
+                                    start_screen=False
+                                    gamemode=False
+                                elif medium_rect.collidepoint(event.pos):
+                                    liste= medium_word
+                                    start_screen=False
+                                    gamemode=False
+                                elif hard_rect.collidepoint(event.pos):
+                                    liste= hard_word
+                                    start_screen=False
+                                    gamemode=False  
+                                random_word= random_word=random.choice(liste)
+                                hide_word="-"*len(random_word) 
+                                lose =0                      
+                            pygame.display.flip()        
             if event.type == pygame.MOUSEBUTTONDOWN:
-                # Si l'utilisateur clique dans la zone de texte, activez l'entrée de texte
+                # Si l'utilisateur clique dans la zone de texte, il pourra ecrire le mot qu'il souhaite ajouter
                 if text_rect.collidepoint(event.pos):
                     input_active = True
                 else:
                     input_active = False
             if event.type == pygame.KEYDOWN:
                 if input_active:
-                    if event.key == pygame.K_RETURN:
+                    if event.key == pygame.K_RETURN: # lorsqu'il appuiras sur entrée le texte sera ecrit dans le fichier texte
                         with open("mots.txt","a") as file:
                             file.write(user_text+"\n")
-                        print(user_text)  # Affiche le texte lorsque l'utilisateur appuie sur Entrée
+                        print(user_text)  
                         user_text = ''  # Efface le texte après avoir appuyé sur Entrée
                     elif event.key == pygame.K_BACKSPACE:
                         user_text = user_text[:-1]  # Supprime le dernier caractère
@@ -78,33 +116,38 @@ while game:
         screen.blit(text_surface, (text_rect.x + 5, text_rect.y + 5))
 
         pygame.display.flip()
-
+    #quand l'utilisateur aura choisi sa difficulté il tombera sur cet event (le jeu du pendu)
     for event in pygame.event.get():
         text_tentatives = font.render("Tentatives: " + ", ".join(sorted(tentatives)), True, (0, 0, 0))
         screen.blit(text_tentatives, (50, 50))
-        
+        screen.blit(start_text, (start_rect.centerx -29 , start_rect.centery -10))
         if event.type == pygame.QUIT:
             sys.exit()    
-        print(random_word) 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if start_rect.collidepoint(event.pos):   
+
+                print(random_word) 
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_ESCAPE:
                 tentatives = set()  # Pour stocker les lettres déjà essayées
-                random_word= random_word=random.choice(mot)
+                random_word= random_word=random.choice(liste)
                 hide_word="-"*len(random_word) 
-                lose =0 
-                win=0
-        if event.type == pygame.KEYDOWN:
+                lose =0
+        if event.type == pygame.KEYDOWN: 
             if event.unicode.isalpha() and event.unicode.upper() not in tentatives:
                 tentatives.add(event.unicode.upper())
-
                 if event.unicode.upper() in random_word.upper():
                     hide_word = "".join([i if i.upper() in tentatives else "-" for i in random_word])
+                    print(hide_word)
+                    print(random_word)
+                    
                 else:
                     lose +=1    
                     print(lose)
-        
-
-        screen.fill((0,255,255))   
+                # if win == len(random_word):
+                #     message("Gagné ! :D ",(500, 150,600,50),(255,0,0))
+        screen.fill((255,255,255))
+        #compte les nombre de fausse lettre de l'utilisateur et les associe aux differentes etapes du pendu (c'est des images)   
         if lose==0:
             screen.blit(image1,(350,200))
         if lose==1:
@@ -121,6 +164,8 @@ while game:
             screen.blit(image7,(350,200))                  
             message("Perdu ! :(",(500, 150,600,50),(255,0,0))    
             message("Appuyez sur échap pour (re)commencer une partie",(225, 50,600,50),(0,0,0)) 
+        if hide_word==random_word:
+            message("Gagné ! :D ",(500, 150,600,50),(255,0,0))
         message("Tentatives: " + ", ".join(sorted(tentatives)),(50, 115,600,50),(0,0,0))       
         message(hide_word,(450, 450,600,50),(0,0,0))
         pygame.display.flip()    
